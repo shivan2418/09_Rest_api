2ad3573ed48c6ee0457473f51bd68f1b26ac9aaf
Emil Elgaard <shivan@shivan.dk>
2020-06-01 13:36:06 -0400

    add app test

--- app.js
+++ app.js
@@ -4,6 +4,9 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const Sequelize = require('sequelize');
+const config = require('./config/config.json')
+
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
@@ -43,6 +46,19 @@ app.use((err, req, res, next) => {
 
 // set our port
 app.set('port', process.env.PORT || 5000);
+
+
+const sequelize = new Sequelize({dialect:'sqlite',storage:"fsjstd-restapi.db"})
+
+const test_connection = (async ()=> {
+  try{
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully!');
+  }catch(error){
+    console.error('Unable to connect to the database:', error);
+  }
+});
+test_connection();
 
 // start listening on our port
 const server = app.listen(app.get('port'), () => {
