4c2fd1fc61a85b5b50c6ec6cd658b81e0120a6f6
Emil Elgaard <shivan@shivan.dk>
2020-06-01 23:09:01 -0400

    Add 404 and 500 error handler

--- app.js
+++ app.js
@@ -37,6 +37,21 @@ app.use((req, res) => {
   });
 });
 
+// catch all not found
+app.use((req,res,next) =>{
+  const err = new Error('Not Found');
+  err.status(404);
+  next(err);
+});
+
+
+app.use((err,req,res,next) =>{
+  res.status(err.status || 500); 
+  res.json({error: {message:err.message}});
+})
+
+
+
 // setup a global error handler
 app.use((err, req, res, next) => {
   if (enableGlobalErrorLogging) {
@@ -49,6 +64,13 @@ app.use((err, req, res, next) => {
   });
 });
 
+
+
+
+
+
+
+
 // set our port
 app.set("port", process.env.PORT || 5000);
 
@@ -59,19 +81,8 @@ app.use((req, res) => {
   });
 });
 
-// global other error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = "Page Not Found!";
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
 // Sync models and test connection
-sequelize.sync()
+sequelize.sync({force:false})
                             .then((res) => {
     sequelize.authenticate().then ( res => {console.log("Connection has been established successfully!")})
                             .catch( res=>  {console.error("Unable to connect to the database:", error)});
